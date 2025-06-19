86d6c11b117c9e5eb0cf0a75394b83b61fbf4336
unknown <reiniermayeargmaill.com>
2025-06-19 10:26:43 -0400

    dfgdfgdfg

--- app/test.py
+++ app/test.py
@@ -1,4 +1,5 @@
-
+import threading
+from flask import Flask, request
 from selenium.webdriver.common.by import By
 import undetected_chromedriver as uc
 import os
@@ -21,12 +22,35 @@ bot.send_message(os.environ["admin"], "Lo pude hacer!")
 bot.send_photo(os.environ["admin"], telebot.types.InputFile("hola.png"), "Aqui está, enviado desde el contenedor Docker")
 
 
-@bot.message_handler(lambda x: True)
+@bot.message_handler(func=lambda x: True)
 def cmd_asd(m):
     bot.send_photo(os.environ["admin"], telebot.types.InputFile("hola.png"), "Aqui está, enviado desde el contenedor Docker")
     return
 
 
-bot.infinity_polling()    
+app = Flask(__name__)
+@app.route('/')
+def index():
+    return "Hello World"
+
+def flask():
+    app.run(host="0.0.0.0", port=5000)
+
+
+
+try:
+    print(f"La dirección del servidor es:{request.host_url}")
+except:
+    hilo_flask=threading.Thread(name="hilo_flask", target=flask)
+    hilo_flask.start()
+
+try:
+    bot.remove_webhook()
+except:
+    pass
+
+
+print("tamo activo papi")
+bot.infinity_polling()  
     
 
